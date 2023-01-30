4ad7851edd3011d6fdc11012bf66f9b364a020ab
Roberto Salazar <roberto.salazar@upaep.mx>
2023-01-30 11:50:35 -0600

    Sesión 6 - 27/01/2023

---
+++ 01 - Javascript/Ejercicio14.js
@@ -0,0 +1,14 @@
+var arreglo = new Array();
+
+arreglo[0] = "Hola!";
+arreglo[3] = "Mundo"
+console.log(arreglo);
+
+var arreglo2 = [
+    1,
+    true,
+    "Hola!",
+    { nombre: "Roberto "}
+];
+
+console.log(arreglo2);
---
+++ 01 - Javascript/Ejercicio15.js
@@ -0,0 +1,12 @@
+var list = [1, 2, 3, 4, 5];
+
+console.log(list);
+console.log(list.toString());
+console.log(list.join('-'));
+console.log(list.concat([6, 7, 8]));
+var arr = list.concat([7, 8, 9]);
+console.log(arr);
+console.log(list.splice(2,2,3));
+console.log(list.slice(2,3));
+console.log(list.indexOf(3));
+console.log(list.lastIndexOf(3));
