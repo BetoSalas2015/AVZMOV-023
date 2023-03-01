59158c03a14e683b54e8d1ccd2971c4608787406
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2023-03-01 12:51:33 -0600

    Sesión 19 - 01/03/2023

--- 03 - Node/app.js
+++ 03 - Node/app.js
@@ -25,29 +25,31 @@ const salarios = [
     }
 ];
 
-const getEmpleado = ( id, callback ) => {
-    const empleado = empleados.find( (e) =>  e.id === id )?.nombre;
-    if (empleado) {
-        callback (null, empleado);
-    } else {
-        callback( `El empleado con id ${id} no existe.`);
-    }
+const getEmpleado = ( id ) => {
+    return new Promise( (resolve, reject) => {
+        const empleado = empleados.find( (e) =>  e.id === id )?.nombre;
+        (empleado) ? resolve(empleado) : reject(`El empleado con id ${id} no existe.`);
+    } );
 };
 
-const getSalario = ( id, callback ) => {
-    const salario = salarios.find( (s) =>  s.id === id )?.salario;
-    if (salario) {
-        callback (null, salario);
-    } else {
-        callback( `El salario para el id ${id} no existe.`);
-    }
+const getSalario = ( id ) => {
+    return new Promise((resolve, reject) => {
+        const salario = salarios.find( (s) =>  s.id === id )?.salario;
+        (salario) ? resolve(salario) : reject(`El salario para el id ${id} no existe.`);
+    });
 };
 
+const id = 4;
+
+getEmpleado(id)
+        .then( (empleado) => { 
+            getSalario(id).then( (salario) => {
+                console.log(`El empleado ${ empleado } tiene el salario ${ salario }`);
+            }).catch( (err) => { console.log(`El empleado ${empleado} no tiene salario.`);})
+        }).catch( (err) => { console.log(err);} );  ;
 
 
-const id = 5;
-
-getEmpleado( id, (err, empleado) => { 
+/*getEmpleado( id, (err, empleado) => { 
     if( err ) {
         return console.log(err);
     }
@@ -57,7 +59,7 @@ getEmpleado( id, (err, empleado) => {
         }
         console.log(`El empleado ${empleado} gana ${salario}.`);
     });
-});
+});*/
 
 
 
